3367106447435cac9ac77358aa1c08d1999fb4e4
baxriddin0317 <baxriddinxojaaxmadaliyev@gmail.com>
2024-10-18 14:17:36 +0500

    intial commit

---
+++ firebase/FirebaseConfig.jsx
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+import { getAuth } from "firebase/auth"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC7hEmfREjZbznFaPGz1-2v3dDmwGxYPTc",
+  authDomain: "ecommerce-195aa.firebaseapp.com",
+  projectId: "ecommerce-195aa",
+  storageBucket: "ecommerce-195aa.appspot.com",
+  messagingSenderId: "1051253166285",
+  appId: "1:1051253166285:web:49d3cad42b82172ff39984"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const fireDB = getFirestore(app);
+const auth = getAuth(app)
+
+export { fireDB, auth }
